088b89441354edd74ade0c35826e6228b06fdf7f
JakubKwiatek132 <126687451+JakubKwiatek132@users.noreply.github.com>
2023-03-15 13:23:46 +0000

    suma na sume potegowania

--- Main.java
+++ Main.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-  Scanner skan = new Scanner(System.in);
+    Scanner skan = new Scanner(System.in);
     System.out.println("Podaj pierwszą liczbe ");
-      int x = skan.nextInt();
-     System.out.println("Podaj drugą liczbe ");
-      int y = skan.nextInt();
-    int suma = x + y;
+    int x = skan.nextInt();
+    System.out.println("Podaj drugą liczbe ");
+    int y = skan.nextInt();
+    int potega1 = (int) Math.pow(x, y);
+    int potega2 = (int) Math.pow(y, x);
+    int suma = potega1 + potega2;
     System.out.println("Wynik: " + suma);
   }
 }
